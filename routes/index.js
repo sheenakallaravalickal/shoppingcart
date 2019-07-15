95a7d298909772d2eff6bb9d108717dc5406676e
sheena k sasi <6154sheena@gmail.com>
2019-07-15 19:47:05 +0530

    signup added some modifications added

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,12 @@ var router = express.Router();
 
 
 let dbConnect=require('../dbconfig/db-connect');
+
+let csrf=require('csurf');
+let csrfProtection=csrf();
+router.use(csrfProtection);
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   dbConnect.get().collection('product').find().toArray(function (err,docs) {
@@ -10,5 +16,13 @@ router.get('/', function(req, res, next) {
 
   });
 });
+
+router.get('/user/signup',function (req,res) {
+      res.render('user/signup',{csrfToken:req.csrfToken})
+})
+router.post('/user/signup',function (req,res) {
+    res.redirect('/')
+
+})
 
 module.exports = router;
